cb22708870e1e7be4fc8d8cc6b83c40a3e93779e
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-06-08 06:39:36 +0300

    Import knightMoves function and test it

---
+++ main.mjs
@@ -0,0 +1,10 @@
+import knightMoves from "./knightMoves.mjs";
+
+
+
+
+const firstMove = knightMoves([0, 0], [3, 3]);
+const secondMove = knightMoves([0, 0], [7, 7]);
+
+console.log(firstMove);
+console.log(secondMove);
