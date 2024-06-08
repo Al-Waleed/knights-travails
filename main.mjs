405bbea30ef77c7b98805ec412a4bd1eab4a64c7
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-06-08 06:47:33 +0300

    Add a pretty print function

--- main.mjs
+++ main.mjs
@@ -1,10 +1,16 @@
 import knightMoves from "./knightMoves.mjs";
 
-
-
+function prettyPrintPath(array) {
+  console.log(
+    `=> You made it in ${array.length - 1} moves!  Here's your path:`
+  );
+  array.forEach((move) => console.log(`-(${move})`) );
+}
 
 const firstMove = knightMoves([0, 0], [3, 3]);
 const secondMove = knightMoves([0, 0], [7, 7]);
+const thirdMove = knightMoves([2,5], [7,6]);
 
-console.log(firstMove);
-console.log(secondMove);
+prettyPrintPath(firstMove);
+prettyPrintPath(secondMove);
+prettyPrintPath(thirdMove);
